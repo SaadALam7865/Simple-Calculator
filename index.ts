09165d4930ae9c4b370371433291b1f77af35835
SaadALam7865 <saadalam929@gmail.com>
2024-04-04 01:22:03 -0700

    First-commit

---
+++ index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// Printing a Welcome Message
+console.log("\n\tWellcome To \'saadalam\' - CLI Simple Calculator\n" );
+
+// Asking Question from users through inquirer
+
+let answers = await inquirer.prompt([
+    {message: "Enter First Number", type: "number", name: "firstNumber" },
+    {message: "Enter Second Number", type: "number", name: "secondNumber"},
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name:"operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+
+// Conditional Statements
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else{
+    console.log("Invalid Input");
+}
